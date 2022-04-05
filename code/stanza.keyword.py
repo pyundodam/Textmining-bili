a5c5565e0fc2fa5578533d476de19adf79927c5c
pyundodam <100251672+pyundodam@users.noreply.github.com>
2022-04-05 12:26:03 +0900

    save

---
+++ code/stanza.keyword.py
@@ -0,0 +1,17 @@
+from encodings import utf_8
+import stanza
+from collections import Counter
+import pandas as pd
+
+stanza.download('zh',processors='tokenize,pos')
+nlp = stanza.Pipeline('zh', processors='tokenize,pos')
+text = nlp("我爱北京天安门")
+
+list = [list.text for list in text.sentences]
+txt = [f'{text.text}' for text in text.sentences for text in text.words]
+pos = [f'{text.xpos}' for text in text.sentences for text in text.words]
+
+# print(frame)
+df = pd.DataFrame({"text":txt,"pos":pos},index=range(0,len(txt)))
+# df.to_excel('stanza_test(2).xlsx')
+print(df)
